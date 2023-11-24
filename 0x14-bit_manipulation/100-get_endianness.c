14587231a6d78c253b11c986aea6e41e8d67d707
flamonrose23 <fatinemar20@icloud.com>
2023-11-24 14:47:23 -0800

    Task 6 adv

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * get_endianness - checking if machine is endianness
+ * Return: 0 if big endian, 1 if little endian
+*/
+int get_endianness(void)
+{
+	unsigned int x = 1;
+	char *c = (char *) &x;
+
+	return (*c);
+}
