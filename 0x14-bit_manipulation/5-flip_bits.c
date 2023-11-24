6d1fb04075f5a9e353e1e6b86a22da5487742c84
flamonrose23 <fatinemar20@icloud.com>
2023-11-24 14:23:41 -0800

    Task 5 correction

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -13,7 +13,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned long int current;
 	unsigned long int exclusive = n ^ m;
 
-	for (x = 63; x >= 0; x—)
+	for (x = 63; x >= 0; x--)
 	{
 		current = exclusive >> x;
 		if (current & 1)
