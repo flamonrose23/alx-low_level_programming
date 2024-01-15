7b5c0e152dc20f6c6b9660ed73feef0ae9fde421
flamonrose23 <fatinemar20@icloud.com>
2024-01-15 14:54:08 -0800

    Task 2. Let's call C functions from Python

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int add(int x, int y)
+{
+	return (x + y);
+}
+
+int sub(int x, int y)
+{
+	return (x - y);
+}
+
+int mul(int x, int y)
+{
+	return (x * y);
+}
+int div(int x, int y)
+{
+	return (x / y);
+}
+int mod(int x, int y)
+{
+	if (y ==  0)
+	{
+		printf("Error: Division by zero\n");
+		return (0);
+	}
+	return (x % y);
+}
+
