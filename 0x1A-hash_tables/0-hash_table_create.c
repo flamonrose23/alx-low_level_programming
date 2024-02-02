5a5c80a1c6cfd38fa1a3a7d4f1e25fb1983a08ee
flamonrose23 <fatinemar20@icloud.com>
2024-02-02 11:33:25 -0800

    Testing task 0 of hash_table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_table_create - writing function that creates hash table
+ * @size: refering to the size of the hash table created
+ * Return: NULL or pointer to the newly created hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_newtable2;
+
+	hash_newtable2 = malloc(sizeof(hash_table_t));
+	if (!hash_newtable2)
+		return (NULL);
+	hash_newtable2->array = malloc(sizeof(hash_node_t *) * size);
+	if (!hash_newtable2->array)
+	{
+		free(hash_newtable2);
+		return (NULL);
+	}
+	hash_newtable2->size = size;
+	return (hash_newtable2);
+}
