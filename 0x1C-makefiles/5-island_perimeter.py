129f791fe9e6f824cf391f63585f2810b99fc896
flamonrose23 <fatinemar20@icloud.com>
2024-03-20 18:50:10 -0700

    Task 5 testing

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""Creating function about island perimeter"""
+
+def island_perimeter(grid):
+    """
+    Returning island's perimeter described in grid
+    Grid represented water by 0 and land by 1
+    Grid is rectangular, width and height don’t exceed 100
+    Grid is completely surrounded by water, and there is one island (or nothing)
+    """
+
+    width = len(grid[0]) ; height = len(grid)
+    size = 0 ; edges = 0
+
+    for a in range(height):
+        for b in range(width):
+            if grid[a][b] == 1:
+                size += 1
+                if (a > 0 and grid[a - 1][b] == 1):
+                    edges += 1
+                if (b > 0 and grid[a][b - 1] == 1):
+                    edges += 1
+    return (size * 4 - edges * 2) 
